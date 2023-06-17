714a8d3f17f405311fd4e370040973a068547411
Gokul-96 <gokulakrishnan.m96@gmail.com>
2023-06-17 22:42:59 +0530

    task6

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
-//Task4
-
-// The useState hook is imported from the 'react' library along with the React module.
-// Inside the MyComponent function, the useState hook is used to declare a state variable called count with an initial value of 0.
-// The useState hook returns an array with two elements: the current state value (count) and a function to update the state (setCount).
-// The handleClick function is an event handler for a button click, and it updates the count state by calling the setCount function and passing the new value (count + 1).
-// The current state value (count) is displayed in the UI using curly braces {}.
-// When the button is clicked, the handleClick function is called, which triggers the state update and causes the component to re-render.
-// By using the useState hook, you can manage and update state in functional components in React.
-
+//Task6
+//Use button in one component
+//state should in App component only
+//statics are in one component
+//and staticlines are in one component
 
 import { useState } from 'react'
+
+import React from 'react'
+
+
+
+
+function StaticsLine({text,value}) {
+  return (
+    <div>
+<p>{text}{value}</p>
+    </div>
+  )
+}
+
+function Statics({good,neutral, bad }) {
+  return (
+    <div>
+    <h1>statics</h1>
+            <StaticsLine text='good' value ={good} />
+            
+            <StaticsLine text='neutral' value ={neutral} />
+            
+            <StaticsLine text='bad' value ={bad} />
+
+            
+            <StaticsLine text='all' value ={good + neutral + bad} />
+            <StaticsLine text='average' value ={(good + neutral + bad)/3}/>
+            <StaticsLine text='positivity' value ={(good/(good + neutral + bad))*100 + '%'}/>
+       
+    
+        </div>
+  )
+}
+
+
+
+function Button({handleClick, text}) {
+  return (
+    <div>
+     <button onClick={handleClick}>{text}</button>
+
+      </div>
+
+    
+  )
+}
+
+
+
      
 function App() {
 
@@ -24,20 +68,16 @@ function App() {
     <div>
       <div>
         <h1>Give feedback</h1>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad + 1)}>bad</button>
+        <Button handleClick={()=> setGood(good+1)} text='good' />
+        <Button handleClick={()=> setNeutral(neutral+1)} text='neutral' />
+        <Button handleClick={()=> setBad(bad+1)} text='bad' />
+
       </div>
 
 
       <div>
-        <h1>statics</h1>
-        <p>good{good}</p>
-        <p>neutral{neutral}</p>
-        <p>bad{bad}</p>
-        <p>all {good + neutral + bad}</p>
-        <p>average{(good + neutral + bad)/3}</p>
-        <p>positivity{(good/(good + neutral + bad))*100}%</p>
+        <Statics good={good} neutral={neutral} bad={bad} />
+       
 
       </div>
     </div>
@@ -45,6 +85,7 @@ function App() {
 }
 
 export default App
+
 
 
 
